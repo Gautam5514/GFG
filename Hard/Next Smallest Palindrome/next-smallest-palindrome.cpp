a86d5376cc6a2965e4b354361888ec1b2763e67e
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2023-08-17 22:23:35 +0530

    Updated Solution - GfG to GitHub

--- Hard/Next Smallest Palindrome/next-smallest-palindrome.cpp
+++ Hard/Next Smallest Palindrome/next-smallest-palindrome.cpp
@@ -9,37 +9,45 @@ class Solution{
 public:
 	vector<int> generateNextPalindrome(int num[], int n) {
 	    // code here
-	      bool change=1;
-        for(int i=0;i<n/2;i++){
-            if(num[i]>num[n-i-1]){
-                change=0;
-            }
-            else if(num[i]<num[n-i-1])change=1;
-            
-            num[n-i-1]=num[i];
+	    bool change = 1;
+        for(int i = 0; i < n / 2; i++){
+            if(num[i] < num[n - i - 1])
+                change = 1;
+            else if(num[i] > num[n - i - 1])
+                change = 0;
+                
+            num[n - i - 1] = num[i];
         }
-        vector<int>ans(n);
-        for(int i=0;i<n;i++)ans[i]=num[i];
-        int carry=change;
-        int ind=n/2;
-        while(carry and ind<n){
-            if(num[ind]==9){
-                ans[ind]=0;
-                ans[n-ind-1]=0;
+        
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++)
+            ans[i] = num[i];
+            
+        int ind = n / 2;
+        
+        bool carry = change;
+        while(carry and ind < n){
+            if(ans[ind] == 9){
+               ans[ind] = 0;
+               ans[n - ind - 1] = 0;
             }
             else{
-                ans[ind]++;
-                ans[n-ind-1]=ans[ind];
-                carry=0;
+               ++ans[ind];
+               ans[n - ind - 1] = ans[ind];
+               carry = 0;
             }
-            ind++;
+            
+            ++ind;
         }
+        
         if(carry){
-            ans[0]=1;
-            ans.push_back(1);
+           ans[0] = 1;
+           ans.push_back(1);
         }
+            
         return ans;
 	}
+
 
 };
 
