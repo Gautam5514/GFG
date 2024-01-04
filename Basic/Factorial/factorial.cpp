e2cf0d3b670b500d533c602b4b75d1c9236b5c72
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-01-04 08:15:48 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Factorial/factorial.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int N){
+        long long int fact = 1;
+        if(N == 0 || N == 1) {
+            return fact;
+        }
+        fact = N;
+        for(int i = N-1; i > 1; i--) {
+            fact = fact*i;
+        }
+        return fact;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
