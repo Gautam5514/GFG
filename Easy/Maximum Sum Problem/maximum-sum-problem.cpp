3563abdbec6212900ae144eaae15c2bb9c5a955c
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-02-24 21:46:51 +0530

    Updated Solution - GfG to GitHub

--- Easy/Maximum Sum Problem/maximum-sum-problem.cpp
+++ Easy/Maximum Sum Problem/maximum-sum-problem.cpp
@@ -6,21 +6,17 @@ using namespace std;
 class Solution
 {
     public:
-    int fun(int n, vector<int> &dp) {
-        if(n <= 11) {
-            return (n);
-        }
-        if(dp[n]!= -1) {
-            return (dp[n]);
-        }
-        return (dp[n] = max(n, fun(n/2, dp) +fun(n/3, dp) + fun(n/4, dp)));
+    int solve(int n, vector<int>&dp ) {
+        if(n == 0 || n == 1) return n;
+        if(dp[n] != -1) return dp[n];
+        
+        return dp[n] = max(n, solve(n/2, dp)+solve(n/3, dp)+ solve(n/4, dp));
     }
         int maxSum(int n)
         {
             //code here.
-            vector<int> dp(n+1, -1);
-            return (fun(n, dp));
-            
+            vector<int> dp(n +1, -1);
+            return solve(n, dp);
         }
 };
 
