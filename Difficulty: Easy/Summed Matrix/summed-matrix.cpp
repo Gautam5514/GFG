265cc7646ba28dec9d9bcf09718535c2f0434590
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-06-24 23:56:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Summed Matrix/summed-matrix.cpp
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -10,11 +10,13 @@ class Solution {
     long long sumMatrix(long long n, long long q) {
         // code here
         if(q < 2 || q > 2 * n) {
-            return 0;
+            return 0; // if q is out of range return 0
         }
+        // Calculate the range of i and j
         long long max_i = max(1LL, q - n);
         long long min_i = min(q - 1, n);
         
+        // calculate the number of valid (i, j) pairs
         long long count = max_i <= min_i ? (min_i - max_i + 1) : 0;
         
         return count;
