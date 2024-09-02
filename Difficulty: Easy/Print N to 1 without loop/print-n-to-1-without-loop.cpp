73066e9457823babc5cc2950a1d6e9431ec0e03a
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-09-02 19:57:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.cpp
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.cpp
@@ -8,11 +8,11 @@ class Solution {
   public:
     void printNos(int N) {
         // code here
-        if(N == 0) {
+        if( N == 0 ) {
             return;
         }
         cout << N << " ";
-        printNos(N - 1);
+        printNos(N - 1 );
         
     }
 };
