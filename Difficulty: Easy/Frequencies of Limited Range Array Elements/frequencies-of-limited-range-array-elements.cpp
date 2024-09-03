67c5113f800309213e5b8d2e9032840c3673ca85
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-09-04 03:25:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr, int N, int P) {
+        // do modify in the given array
+        unordered_map<long long , int> mp;
+        for(long long i = 0; i < N; i++) {
+            mp[arr[i]]++;
+        }
+        // traverse in the freq of all no. if present then give the freq 
+        for(int i = 0; i < N; i++) {
+            arr[i] = mp[i+1]; // print the frequency of array
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+
+    // testcases
+    cin >> t;
+
+    while (t--) {
+
+        int N, P;
+        // size of array
+        cin >> N;
+
+        vector<int> arr(N);
+
+        // adding elements to the vector
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        cin >> P;
+        Solution ob;
+        // calling frequncycount() function
+        ob.frequencyCount(arr, N, P);
+
+        // printing array elements
+        for (int i = 0; i < N; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
