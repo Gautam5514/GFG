e93535a802c661cf72a396bbc6b3e73a9f2a7d92
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-11-14 07:28:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        // code
+        int n = arr.size();
+        
+        // using the priority queue minheap
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        for(int i = 0; i <= k && i < n; i++) {
+            minHeap.push(arr[i]);
+        }
+        
+        int index = 0;
+        // for the rest of the element of the array
+        for(int i = k + 1; i < n; i++) {
+            arr[index++] = minHeap.top();
+            minHeap.pop();
+            minHeap.push(arr[i]);
+        }
+        while(!minHeap .empty()) {
+            arr[index++] = minHeap.top();
+            minHeap.pop();
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        obj.nearlySorted(arr, k);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
