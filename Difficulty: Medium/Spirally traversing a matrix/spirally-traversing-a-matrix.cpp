ce750de8a6fab85b4c71e6701abfa3b31e98d034
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2024-12-20 23:58:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -6,44 +6,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
-        // code here
-        int n =   matrix.size();
-        int m = matrix[0].size();
-        int left = 0, right = m-1;
-        int top = 0, bottom = n-1;
-        vector<int> ans;
-        
-        // for check while left & right | top & bottom 
-        while(left <= right && top <= bottom) {
-            // left to right
-            for(int i = left; i <= right; i++) {
-                ans.push_back(matrix[top][i]);
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
+     int row=0,col=0,srow=0,scol=0,erow = mat.size() - 1, ecol = mat[0].size() - 1; // End row and column
+        vector<int>ans;
+       while (srow <= erow && scol <= ecol) {
+            for(int j=scol;j<=ecol;j++){
+                ans.push_back(mat[srow][j]);
             }
-            top++;
-            // top to bottom
-            for(int i = top; i <= bottom; i++) {
-                ans.push_back(matrix[i][right]);
+            srow++;
+            for(int j=srow;j<=erow;j++){
+                ans.push_back(mat[j][ecol]);
             }
-            right--;
-            // right to left
-            if(top <= bottom) {
-            for(int i = right; i >= left; i--) {
-                ans.push_back(matrix[bottom][i]);
+            ecol--;
+               if (srow <= erow) {
+            for(int j=ecol;j>=scol;j--){
+                ans.push_back(mat[erow][j]);
             }
-            bottom--;
+            erow--;
+               }
+                 if (scol <= ecol) {
+            
+              for(int j=erow;j>=srow;j--){
+                ans.push_back(mat[j][scol]);
             }
-            // bottom to top
-            if(left <= right){
-            for(int i = bottom; i >= top; i--) {
-                ans.push_back(matrix[i][left]);
-            }
-            left++;
-        }
+            scol++;
+                 }
+            
         }
         return ans;
         
-    }
+}
+
 };
 
 //{ Driver Code Starts.
@@ -54,9 +47,10 @@ int main() {
     while (t--) {
         int r, c;
         cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        vector<vector<int>> matrix(r);
 
         for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
             for (int j = 0; j < c; j++) {
                 cin >> matrix[i][j];
             }
@@ -67,6 +61,9 @@ int main() {
         for (int i = 0; i < result.size(); ++i)
             cout << result[i] << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
