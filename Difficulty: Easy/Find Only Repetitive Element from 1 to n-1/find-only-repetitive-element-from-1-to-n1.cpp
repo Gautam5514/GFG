d5675b1f4c8598ed34988bd2dc23887b3bd431f2
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-04-20 23:35:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int findDuplicate(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        unordered_map<int, int> mp;
+        
+        for(auto it : arr) {
+            mp[it]++;
+        
+        if(mp[it] == 2) {
+            return it;
+        }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        int result = ob.findDuplicate(arr);
+
+        cout << (result) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
