4472c757594e9de1317d7f852467bd1bed0afd4d
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-04-23 11:48:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number II/unique-number-ii.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> singleNum(vector<int>& arr) {
+        // Code here.
+        
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        
+        for(auto it : arr) {
+            mp[it]++;
+        }
+        
+        for(auto num : mp) {
+            if(num.second == 1) {
+                ans.push_back(num.first);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.singleNum(arr);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
