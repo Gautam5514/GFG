76ae926f3fdfe88d425f3b2ae4d85962606bbbcb
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-06-23 23:47:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.cpp
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -1,77 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    string minSum(vector<int> &arr) {
-        // code here
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
+    string addStrings(string num1, string num2) {
+        string result;
+        int i = num1.size() - 1, j = num2.size() - 1, carry = 0;
         
-        string s1 = "";
-        string s2 = "";
-        
-        for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) {
-                s1 += to_string(arr[i]);
-            } else {
-                s2 += to_string(arr[i]);
-            }
-        }
-        reverse(s1.begin(), s1.end());
-        reverse(s2.begin(), s2.end());
-        
-        string ans = "";
-        int i = 0;
-        int carry = 0;
-        while(i < s1.size() || i < s2.size() || carry) {
-            int sum = 0;
-            if(i < s1.size())
-            sum += s1[i] - '0';
-            if(i < s2.size())
-            sum += s2[i] - '0';
-            
-            sum += carry;
-            ans += to_string(sum % 10);
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += num1[i--] - '0';
+            if (j >= 0) sum += num2[j--] - '0';
             carry = sum / 10;
-            i++;
-        } 
-        while(ans.back() == '0') {
-            ans.pop_back();
+            result += (sum % 10) + '0';
         }
-        reverse(ans.begin(), ans.end());
+        reverse(result.begin(), result.end());
+        return result;
+    }
+
+    string minSum(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+        string a = "", b = "";
         
-        return ans;
+        for (int i = 0; i < arr.size(); i++) {
+            if(arr[i] == 0){
+                continue;
+            }
+            if (i % 2 == 0) a += to_string(arr[i]);
+            else b += to_string(arr[i]);
+        }
+
+        return addStrings(a, b);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.minSum(a);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
