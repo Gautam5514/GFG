f7a0f3cb82eb163ddd5db31402418b378647e92f
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-07-25 09:43:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -1,70 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // arr: input array
-    // Function to find maximum circular subarray sum.
-    // find the maximum subarray using kadane algo
-    int kadaneMax(vector<int> arr) {
-        int maxEnd = 0, maxSoFar = INT_MIN;
-        for(int num : arr) {
-            maxEnd = max(num, maxEnd + num);
-            maxSoFar = max(maxSoFar, maxEnd);
-        }
-        return maxSoFar;
-    }
-    
-    int kadaneMin(vector<int> arr) {
-        int minEnd = 0, minSoFar = INT_MAX;
-        for(int num : arr) {
-            minEnd = min(num, minEnd + num);
-            minSoFar = min(minSoFar, minEnd);
-        }
-        return minSoFar;
-    }
-    int circularSubarraySum(vector<int> &arr) {
-        int maxKadane = kadaneMax(arr);// straight max arr sum
-        int totalSum = accumulate(arr.begin(), arr.end(), 0); //total array
-        int minKadane = kadaneMin(arr); // min subarray sum
+    int maxCircularSum(vector<int> &arr) {
+        // code here
+        int total = 0;
         
-        // all element are negative
-        if(totalSum == minKadane) {
-            return maxKadane;
+        int maxSum = arr[0], curMax = 0;
+        int minSum = arr[0], curMin = 0;
+        
+        for(auto a : arr) {
+            
+            // find the max
+            
+            curMax = max(a, curMax + a);
+            maxSum = max(maxSum, curMax);
+            
+            // find the min
+            
+            curMin = min(a, curMin + a);
+            minSum = min(minSum, curMin);
+            
+            total += a;
         }
-        // max circular array
-        int circularMax = totalSum - minKadane;
-        return max(maxKadane, circularMax);
+        
+        if(maxSum < 0) return maxSum;
+        
+        return max(maxSum, total - minSum);
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.circularSubarraySum(arr);
-
-        cout << res << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
