e78a98420e3f846da1094a3ba916f424af290c6a
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-08-02 23:45:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -1,59 +1,30 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
 class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
-        int n = arr.size();
-        int ans = 0;
-        int cnt = 0;
+        // Code here
+         int n=(int)arr.size();
+        int res=0;
         
-        unordered_map <int, int> mp;
-        for(int i = -n-1; i <= n+1; i++) mp[i] = -1;
-        
-        mp[0] = 0;
-        for(int i = 0; i < n; i++){
-            cnt += (arr[i] > k);
-            cnt -= (arr[i] <= k);
+        map<int,int>mp;
+        mp[0]=-1;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>k)
+            sum++;
+            else
+            sum--;
             
-            if(mp[cnt] == -1) mp[cnt] = i;
-            if(cnt > 0) ans = i+1;
-            else if(mp[cnt-1] != -1) ans = max(ans, i - mp[cnt-1]);
+            if(mp.find(sum)==mp.end())
+            mp[sum]=i;
+            
+            if(mp.find(sum-1)!=mp.end())
+            res=max(res,i-mp[sum-1]);
+            
+            if(sum>0)
+            res=max(res,i+1);
+            
         }
-        
-        return ans;
+        return res;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        int k = stoi(input);
-
-        Solution ob;
-        cout << ob.longestSubarray(arr, k) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
