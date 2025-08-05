02986508400deed7b3388079fd999ff33860e1ae
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-08-05 23:53:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    bool isPalinSent(string &s) {
+        // code here
+        int left = 0, right = s.length() - 1;
+        
+        while(left < right) {
+            while(left < right && !isalnum(s[left])) left++;
+            
+            while(left < right && !isalnum(s[right])) right--;
+            
+            if(tolower(s[left]) != tolower(s[right])) return false;
+            
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
