c270711c111b0a166095d428e26251853626a4e8
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-08-15 23:31:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.cpp
+++ Difficulty: Medium/Insert Interval/insert-interval.cpp
@@ -1,62 +1,27 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
     vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
                                        vector<int> &newInterval) {
         // code here
-         intervals.push_back(newInterval);
+           intervals.push_back(newInterval);
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> v;
-        for(const auto& i:intervals)
-        {
-            if(v.empty() || v.back()[1]<i[0])
-            {
-                v.push_back(i);
-            }
-            else if(v.back()[1]>=i[0] && v.back()[1]<i[1])
-            {
-                v.back()[1]=i[1];
+        vector<vector<int>> ans;
+        ans.push_back({intervals[0][0],intervals[0][1]});
+        int index=0;
+        
+        //cout<<intervals[0][0]<<" "<<intervals[0][1]<<endl;
+        
+        for(int i=1;i<intervals.size();i++){
+            
+            //cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
+            
+            if(intervals[i][0]<=ans[index][1]){
+                ans[index][1]=max(ans[index][1],intervals[i][1]);
+            }else{
+                ans.push_back(intervals[i]);
+                index++;
             }
         }
-        return v;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<vector<int>> intervals(N, vector<int>(2));
-        for (int i = 0; i < N; i++) {
-            cin >> intervals[i][0] >> intervals[i][1];
-        }
-        vector<int> newInterval(2);
-        cin >> newInterval[0] >> newInterval[1];
-
-        Solution obj;
-        vector<vector<int>> ans = obj.insertInterval(intervals, newInterval);
-        cout << "[";
-        for (int i = 0; i < ans.size(); i++) {
-            cout << "[" << ans[i][0] << ',' << ans[i][1] << ']';
-            if (i != (ans.size() - 1))
-                cout << ",";
-        }
-        cout << "]" << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
