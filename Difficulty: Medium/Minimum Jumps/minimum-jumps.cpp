d860f233dabe41a68655240b89ec4f812f416de0
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-09-11 23:54:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,58 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int minJumps(vector<int>& arr) {
-          int n=arr.size();
-        if(arr[0]==0) return -1;
-        
-        int fuel=arr[0], jump=1, maxi=arr[0];
-        
-        for(int i=1; i<n; i++){
-            if (i == n - 1) return jump;
-            fuel--;
-            maxi=max(maxi, i+arr[i]);
-            if(fuel==0){
-                if(maxi<=i) return -1;
-                else{
-                    fuel=maxi-i;
-                    jump++;
-                }
+        // code here
+           int n=arr.size();
+        int maxi=0;
+        int choice=0;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            maxi=max(maxi,arr[i]+i);
+            if(i==choice){
+                choice=maxi;
+                ans++;
             }
         }
-        return jump;
-        
+        if(choice>=n-1){
+            return ans;
+        }
+        return -1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, i, j;
-        vector<int> arr;
-        string ip;
-        int number;
-        getline(cin, ip);
-        stringstream ss(ip);
-
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.minJumps(arr) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
