0dc70c7fb7760e7ec2ca35c306288a10771b89e3
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-09-12 23:17:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -1,50 +1,21 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
     int getMinDiff(vector<int> &arr, int k) {
+        // code here
+         sort(arr.begin(), arr.end());
         int n = arr.size();
-        sort(arr.begin(),arr.end());
-        int min_diff = arr[n-1] - arr[0];
-        for(int i = 1; i < n; i++){
-            if(arr[i] < k) continue;
-            int mini = min(arr[0] + k, arr[i] - k);
-            int maxi = max(arr[n-1] - k, arr[i-1] + k);
-            min_diff = min(min_diff, maxi - mini);
+        int smallest = arr[0] + k;
+        int largest  = arr[n-1] - k;
+
+        int ans = arr[n-1] - arr[0];
+        for(int i = 0; i < n-1; i++)
+        {
+            int minimum = min(smallest, arr[i+1] - k);
+            int maximum = max(largest, arr[i] +k);
+            if(minimum < 0)
+                continue;
+            ans = min(ans,maximum-minimum);
         }
-        return min_diff;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, k;
-        cin >> k;
-        cin.ignore();
-        vector<int> a, b, c, d;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution ob;
-        auto ans = ob.getMinDiff(a, k);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
