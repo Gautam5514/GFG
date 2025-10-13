5319d4414491ba5106f53f4e59b6f138bf0f0e3c
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2025-10-13 09:42:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Non-Adjacent Nodes Sum/maximum-non-adjacent-nodes-sum.cpp
+++ Difficulty: Medium/Maximum Non-Adjacent Nodes Sum/maximum-non-adjacent-nodes-sum.cpp
@@ -14,17 +14,17 @@ public:
 
 class Solution {
   public:
-  unordered_map<Node*, int> dp;
+  unordered_map<Node*, int> mp;
   int sum(Node* root) {
-      if(root == NULL ) return 0;
-      if(dp[root]) return dp[root];
-      int ll = (root->left != NULL) ? sum(root->left->left) : 0;
-      int lr = (root->left != NULL) ? sum(root->left->right) : 0;
-      int rl = (root->right != NULL) ? sum(root->right->left) : 0;
-      int rr = (root->right != NULL) ? sum(root->right->right) : 0;
-      int take = (root->data) + ll + lr + rl + rr;
+      if(root == NULL) return 0;
+      if(mp[root]) return mp[root];
+      int ll = (root -> left != NULL) ? sum(root -> left -> left): 0;
+      int lr = (root -> left != NULL) ? sum(root -> left -> right) : 0;
+      int rl = (root -> right != NULL) ? sum(root -> right -> left) : 0;
+      int rr = (root -> right != NULL) ? sum(root -> right -> right) : 0;
+      int take = (root -> data) + ll + lr + rl + rr;
       int leave = sum(root->left) + sum(root->right);
-      return dp[root] =  max(take, leave);
+      return mp[root] =  max(take, leave);
   }
     int getMaxSum(Node *root) {
         // code here
