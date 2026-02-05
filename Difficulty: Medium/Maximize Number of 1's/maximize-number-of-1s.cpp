22ae56d6be3948edd368192be75a5bfd0963755b
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2026-02-05 09:28:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
@@ -2,37 +2,29 @@ class Solution {
   public:
     int maxOnes(vector<int>& arr, int k) {
         // code here
-        int n = arr.size();
-        int left = 0, zeroCount = 0, maxLen = 0;
-        
-        for(int right = 0; right < n; right++) {
-            if(arr[right] == 0) zeroCount++;
-            
-            while(zeroCount > k) {
-                if(arr[left] == 0 ) zeroCount--;
-                left++;
-            }
-            maxLen = max(maxLen, right - left + 1);
+          int n = arr.size();
+
+    int left = 0;
+    int zeroCount = 0;
+    int maxLen = 0;
+
+    for (int right = 0; right < n; right++) {
+
+        // If current element is 0 → increase zero count
+        if (arr[right] == 0)
+            zeroCount++;
+
+        // If zeros exceed k → shrink window from left
+        while (zeroCount > k) {
+            if (arr[left] == 0)
+                zeroCount--;
+            left++;
         }
-        return maxLen;
+
+        // Update maximum length of valid window
+        maxLen = max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
     }
 };
-
-
-// int n = arr.size();
-//         int maxLen = 0;
-        
-//         for(int i = 0; i < n; i++) {
-//             int zero = 0;
-            
-//             for(int j = i; j < n; j++) {
-//                 if(arr[j] == 0) {
-//                     zero++;
-//                 }
-//                 if(zero > k) {
-//                     break;
-//                 }
-//                 maxLen = max(maxLen, j - i + 1);
-//             }
-//         }
-//         return maxLen;
