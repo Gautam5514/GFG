47cb7615a32b6e7c07aa220803a434c3d45d407f
Gautam pandit <113331777+Gautam5514@users.noreply.github.com>
2026-03-16 10:04:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sum Paths/k-sum-paths.cpp
+++ Difficulty: Medium/K Sum Paths/k-sum-paths.cpp
@@ -1,87 +1,6 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-// Function to Build Tree
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
 /*
-struct Node {
+class Node {
+  public:
     int data;
     Node *left;
     Node *right;
@@ -94,51 +13,39 @@ struct Node {
 */
 class Solution {
   public:
-  void countPath(Node* root, int k, unordered_map<int, int>& mp, int &count, int currSum) {
-      
-      if(!root) return;
-      
-      currSum += root -> data;
-      
-      if(currSum == k) count++;
-      
-      if(mp.find(currSum - k) != mp.end()) {
-          count += mp[currSum - k];
-        }
-          mp[currSum]++;
-         
-       countPath(root -> left, k, mp, count, currSum);
-       countPath(root -> right, k, mp, count, currSum);
-       
-       mp[currSum]--;
-  }
-    int sumK(Node *root, int k) {
-        unordered_map<int, int> mp;
+  
+    int dfs(Node* root, long long currSum, int k, unordered_map<long long,int>& mp){
+        
+        if(root == NULL) return 0;
+        
+        currSum += root->data;
+        
         int count = 0;
-        countPath(root, k, mp, count, 0);
+        
+        // check if prefix exists
+        if(mp.find(currSum - k) != mp.end()){
+            count += mp[currSum - k];
+        }
+        
+        // store current sum
+        mp[currSum]++;
+        
+        // explore left and right
+        count += dfs(root->left, currSum, k, mp);
+        count += dfs(root->right, currSum, k, mp);
+        
+        // backtrack
+        mp[currSum]--;
+        
         return count;
     }
-};
-
-//{ Driver Code Starts.
-
-int main() {
-    string tc;
-    getline(cin, tc);
-    int t = stoi(tc);
-
-    while (t--) {
-        string s, ch;
-        getline(cin, s);
-        Node *root = buildTree(s);
-
-        string key;
-        getline(cin, key);
-        int k = stoi(key);
-        Solution ob;
-        cout << ob.sumK(root, k) << "\n";
-        cout << "~\n";
+  
+    int countAllPaths(Node *root, int k) {
+        
+        unordered_map<long long,int> mp;
+        
+        mp[0] = 1;   // important base case
+        
+        return dfs(root, 0, k, mp);
     }
-    return 0;
-}
-// } Driver Code Ends
+};
